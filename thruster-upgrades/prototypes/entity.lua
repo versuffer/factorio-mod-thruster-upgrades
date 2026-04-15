27d5fd68160c70a21bb7a6b9a6b7e3451f2115a2
Sergey <versuffer@Sergeys-MBP.IGD_Rostelecom>
2026-04-16 01:09:48 +0300

    Settings (2.2.0)

--- thruster-upgrades/prototypes/entity.lua
+++ thruster-upgrades/prototypes/entity.lua
@@ -10,10 +10,10 @@ thruster_mk2.corpse = "thruster-mk2-remnants"
 thruster_mk2.max_health = data.raw["thruster"]["thruster"].max_health * 1.4
 thruster_mk2.minable.result = "thruster-mk2"
 
-thruster_mk2.max_performance.fluid_usage = data.raw["thruster"]["thruster"].max_performance.fluid_usage * 1.7
-thruster_mk2.max_performance.effectivity = data.raw["thruster"]["thruster"].max_performance.effectivity + 0.1
-thruster_mk2.fuel_fluid_box.volume = data.raw["thruster"]["thruster"].fuel_fluid_box.volume * 2
-thruster_mk2.oxidizer_fluid_box.volume = data.raw["thruster"]["thruster"].oxidizer_fluid_box.volume * 2
+thruster_mk2.max_performance.fluid_usage = settings.startup["thruster-upgrades-thruster-mk2-full-storage-fluid-usage-percent"].value / 100
+thruster_mk2.max_performance.effectivity = settings.startup["thruster-upgrades-thruster-mk2-full-storage-efficiency-percent"].value / 100
+thruster_mk2.fuel_fluid_box.volume = settings.startup["thruster-upgrades-thruster-mk2-storage-volume-units"].value
+thruster_mk2.oxidizer_fluid_box.volume = settings.startup["thruster-upgrades-thruster-mk2-storage-volume-units"].value
 
 -- Thruster MK3 entitites
 
@@ -26,10 +26,10 @@ thruster_mk3.corpse = "thruster-mk3-remnants"
 thruster_mk3.max_health = data.raw["thruster"]["thruster"].max_health * 1.8
 thruster_mk3.minable.result = "thruster-mk3"
 
-thruster_mk3.max_performance.fluid_usage = data.raw["thruster"]["thruster"].max_performance.fluid_usage * 2.5
-thruster_mk3.max_performance.effectivity = data.raw["thruster"]["thruster"].max_performance.effectivity + 0.3
-thruster_mk3.fuel_fluid_box.volume = data.raw["thruster"]["thruster"].fuel_fluid_box.volume * 3
-thruster_mk3.oxidizer_fluid_box.volume = data.raw["thruster"]["thruster"].oxidizer_fluid_box.volume * 3
+thruster_mk3.max_performance.fluid_usage = settings.startup["thruster-upgrades-thruster-mk3-full-storage-fluid-usage-percent"].value / 100
+thruster_mk3.max_performance.effectivity = settings.startup["thruster-upgrades-thruster-mk3-full-storage-efficiency-percent"].value / 100
+thruster_mk3.fuel_fluid_box.volume = settings.startup["thruster-upgrades-thruster-mk3-storage-volume-units"].value
+thruster_mk3.oxidizer_fluid_box.volume = settings.startup["thruster-upgrades-thruster-mk3-storage-volume-units"].value
 
 data.extend(
     {
---
+++ thruster-upgrades/settings.lua
@@ -0,0 +1,68 @@
+-- Thruster MK2 settings
+
+local thruster_mk2_full_storage_efficiency_percent_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk2-full-storage-efficiency-percent",
+    setting_type = "startup",
+    minimum_value = 51,
+    maximum_value = 100,
+    default_value = 61
+}
+
+local thruster_mk2_full_storage_fluid_usage_percent_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk2-full-storage-fluid-usage-percent",
+    setting_type = "startup",
+    minimum_value = 200,
+    maximum_value = 1000,
+    default_value = 340
+}
+
+local thruster_mk2_storage_volume_units_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk2-storage-volume-units",
+    setting_type = "startup",
+    minimum_value = 1000,
+    maximum_value = 50000,
+    default_value = 2000
+}
+
+-- Thruster MK3 settings
+
+local thruster_mk3_full_storage_efficiency_percent_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk3-full-storage-efficiency-percent",
+    setting_type = "startup",
+    minimum_value = 51,
+    maximum_value = 100,
+    default_value = 81
+}
+
+local thruster_mk3_full_storage_fluid_usage_percent_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk3-full-storage-fluid-usage-percent",
+    setting_type = "startup",
+    minimum_value = 200,
+    maximum_value = 1000,
+    default_value = 500
+}
+
+local thruster_mk3_storage_volume_units_setting = {
+    type = "int-setting",
+    name = "thruster-upgrades-thruster-mk3-storage-volume-units",
+    setting_type = "startup",
+    minimum_value = 1000,
+    maximum_value = 50000,
+    default_value = 3000
+}
+
+data.extend(
+    {
+        thruster_mk2_full_storage_efficiency_percent_setting,
+        thruster_mk2_full_storage_fluid_usage_percent_setting,
+        thruster_mk2_storage_volume_units_setting,
+        thruster_mk3_full_storage_efficiency_percent_setting,
+        thruster_mk3_full_storage_fluid_usage_percent_setting,
+        thruster_mk3_storage_volume_units_setting,
+    }
+)
