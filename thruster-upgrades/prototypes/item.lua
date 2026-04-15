b1b30df5becb69896ebcbf96e8287f093d3d861a
Sergey <versuffer@Sergeys-MBP.IGD_Rostelecom>
2026-04-15 16:50:01 +0300

    Minor changes (2.1.1)

--- thruster-upgrades/prototypes/item.lua
+++ thruster-upgrades/prototypes/item.lua
@@ -4,7 +4,7 @@ local thruster_mk2_item = table.deepcopy(data.raw.item["thruster"])
 thruster_mk2_item.name = "thruster-mk2"
 thruster_mk2_item.icon = "__thruster-upgrades__/graphics/item/thruster-mk2-item.png"
 thruster_mk2_item.place_result = "thruster-mk2"
-thruster_mk2_item.order = "z[thruster]"
+thruster_mk2_item.order = "f[thruster]"
 thruster_mk2_item.subgroup = "space-platform"
 
 -- Thruster MK3 items
@@ -13,7 +13,7 @@ local thruster_mk3_item = table.deepcopy(data.raw.item["thruster"])
 thruster_mk3_item.name = "thruster-mk3"
 thruster_mk3_item.icon = "__thruster-upgrades__/graphics/item/thruster-mk3-item.png"
 thruster_mk3_item.place_result = "thruster-mk3"
-thruster_mk3_item.order = "z[thruster]"
+thruster_mk3_item.order = "f[thruster]"
 thruster_mk3_item.subgroup = "space-platform"
 
 data.extend(
